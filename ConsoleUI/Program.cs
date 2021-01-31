07b06dfae8085b3d436eda14f55506c2acdeb654
talhaaersoy <63413850+talhaaersoy@users.noreply.github.com>
2021-01-31 20:30:10 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+              CarManager cars = new CarManager(new InMemoryCarDal());
+              foreach (var car in cars.GetAll())
+              {
+                  Console.WriteLine(car.Id);
+                 
+              }
+        }
+    }
+}
